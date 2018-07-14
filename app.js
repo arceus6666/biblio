b8335499b60f03996f17c1d24dc8f717f8eccda4
Daniel Mendoza <arceus6666@hotmail.com>
2018-07-14 02:19:50 -0400

    changes

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 // si el archivo es index no necesita ser nombrado
-const api = require('./routes')
-const prodApi = require('./routes/prodRoute')
+const bookApi = require('./routes/book')
+const userApi = require('./routes/user')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/personas', api)
-app.use('/productos', prodApi)
+app.use('/book', bookApi)
+app.use('/user', userApi)
+// app.use('/user', userApi)
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.status(200).send('Todo correcto')
 })
 
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,15 +1,17 @@
 const User = require('../models/user')
 const service = require('../services')
 
-function signUp(req, res) {
+function signUp (req, res) {
   const user = new User({
     email: req.body.email,
-    displayName: req.body.displayName,
+    name: req.body.name,
+    lastName: req.body.lastName,
+    age: req.body.age,
     password: req.body.password
   })
 
   user.save((err) => {
-    if(err) {
+    if (err) {
       res.status(500).send({message: err})
     } else {
       res.status(200).send({token: service.createToken(user)})
